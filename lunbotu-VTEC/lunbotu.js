6d85f275d04f6339a36e74256593fb22cd86b995
VTEC <527409987@qq.com>
2016-10-13 00:05:28 +0800

    轮播图封装基本完成
    轮播图封装基本完成

--- lunbotu-VTEC/lunbotu.js
+++ lunbotu-VTEC/lunbotu.js
@@ -5,29 +5,36 @@ var index=0;
 var timerId;
 var inner_width;
 var num1;
+var origin1;
+var start= function (num,origin,arr) {
+    lunbotu(num,origin,arr)
+    autoMove(num,origin)
+    xiabiao(origin)
+}
 //num代表图片个数    append  appendChild
-function lunbotu(num,origin){
+function lunbotu(num,origin,arr){
     num1=num;
+    origin1=origin
     //轮播图片下标
     $('.'+origin).append("<ul class='lunbotu-ul'></ul>")
 
     //动态创建图片个数个li
     for(var i=0;i<num;i++){
-        $('.lunbotu-ul').append("<li class=pic"+i+"><a href='javascript:'><img src='l"+i+".jpg' alt=''></a></li>")
+        $('.'+origin+'>.lunbotu-ul').append("<li class=pic"+i+"><a href='javascript:'><img src='"+arr[i]+"' alt=''></a></li>")
     }
-    $('.lunbotu-ul').append("<li class=pic"+0+"><a href='javascript:'><img src='l"+0+".jpg' alt=''></a></li>")
-    $('.lunbotu-ul').prepend("<li class=pic"+(i-1)+"><a href='javascript:'><img src='l"+(i-1)+".jpg' alt=''></a></li>")
+    $('.'+origin+'>.lunbotu-ul').append("<li class=pic"+0+"><a href='javascript:'><img src='"+arr[0]+"' alt=''></a></li>")
+    $('.'+origin+'>.lunbotu-ul').prepend("<li class=pic"+(i-1)+"><a href='javascript:'><img src='"+arr[num-1]+"' alt=''></a></li>")
 
     //动态创建图片下标
     $('.'+origin).append("<ul class='lunbotu-xiabiao'></ul>")
     for(var i=0;i<num;i++){
-        $('.lunbotu-xiabiao').append("<li class='xiaobiao"+i+"'></li>")
+        $('.'+origin+'>.lunbotu-xiabiao').append("<li class='xiaobiao"+i+"'></li>")
     }
     $()
     //console.log($('.lunbotu-xiabiao').width())
 
 
-    $('.lunbotu-xiabiao').children().css({
+    $('.'+origin+'>.lunbotu-xiabiao').children().css({
         float:'left',
         margin:'0px 5px 10px 10px',
         width:'15px',
@@ -37,45 +44,45 @@ function lunbotu(num,origin){
     })
     //console.log($('.lunbotu-xiabiao').width())
 
-    $('.lunbotu-xiabiao').css({
+    $('.'+origin+'>.lunbotu-xiabiao').css({
         position: 'absolute',
         bottom:'0px',
         //($('.lunbotu-xiabiao').width()为何是750？？？
         left:'50%',
-        marginLeft:-($('.lunbotu-xiabiao').width()/2)+"px"
+        marginLeft:-($('.'+origin+'>.lunbotu-xiabiao').width()/2)+"px"
     })
 
 
     //获取li的宽度
-    inner_width=$('.lunbotu-ul').children().width();
-    $('.lunbotu-ul').css({
+    inner_width=$('.'+origin+'>.lunbotu-ul').children().width();
+    $('.'+origin+'>.lunbotu-ul').css({
         width:(num+2)*100+"%",
         marginLeft:-inner_width+'px'
     })
 
     //a标签
-    $('.lunbotu-ul').children().children().css({
+    $('.'+origin+'>.lunbotu-ul').children().children().css({
         width:'100%',
         height:'100%'
     })
 
     //img标签
-    $('.lunbotu-ul').children().children().children().css({
+    $('.'+origin+'>.lunbotu-ul').children().children().children().css({
         display:'block',
         width:'100%',
         height:'100%'
     })
 
     //li标签
-    $('.lunbotu-ul').children().css({
+    $('.'+origin+'>.lunbotu-ul').children().css({
         width:(100/(num+2))+'%',
         float:'left'
     })
     //console.log($('.'+origin+' .lunbotu-ul').children())
     //ul宽度需要width完全加载完成才能知道，不然是父元素的750px
     //console.log($('.lunbotu-xiabiao').width())
-    $('.lunbotu-xiabiao').css({
-        marginLeft:-($('.lunbotu-xiabiao').width()/2)+"px"
+    $('.'+origin+'>.lunbotu-xiabiao').css({
+        marginLeft:-($('.'+origin+'>.lunbotu-xiabiao').width()/2)+"px"
     })
 
 
@@ -83,26 +90,26 @@ function lunbotu(num,origin){
 //定时器无法执行带参数的函数，必须用一个不带参数的返回带参数的。
 //num1和num相等，无法从html页面直接获取，在lunbotu方法中获取num。
 function fn(){
-    return timeAuto(num1)
+    return timeAuto(num1,origin1)
 }
 //自动轮播方法
-function timeAuto(num){
+function timeAuto(num,origin){
     index++
-    $('.lunbotu-xiabiao').children().css({
+    $('.'+origin+'>.lunbotu-xiabiao').children().css({
         backgroundColor:'white'
     })
-    $('.lunbotu-xiabiao').children().eq(index).css({
+    $('.'+origin+'>.lunbotu-xiabiao').children().eq(index).css({
         backgroundColor:'black'
     })
-    $('.lunbotu-ul').animate({
+    $('.'+origin+'>.lunbotu-ul').animate({
         marginLeft:(-inner_width*(index+1))+'px'
     },200,'swing', function () {
         if(index===num){
             index=0
-            $('.lunbotu-xiabiao').children().eq(index).css({
+            $('.'+origin+'>.lunbotu-xiabiao').children().eq(index).css({
                 backgroundColor:'black'
             })
-            $('.lunbotu-ul').css({
+            $('.'+origin+'>.lunbotu-ul').css({
                 marginLeft:(-inner_width*(index+1))+'px'
             })
 
@@ -111,40 +118,40 @@ function timeAuto(num){
 }
 
 //自动轮播+下标跟随
-function autoMove(num){
+function autoMove(num,origin){
 //            var index=0;
-    inner_width=$('.lunbotu-ul').children().width();
-    $('.lunbotu-xiabiao').children().eq(0).css({
+    inner_width=$('.'+origin+'>.lunbotu-ul').children().width();
+    $('.'+origin+'>.lunbotu-xiabiao').children().eq(0).css({
         backgroundColor:'black'
     })
-    var a=num+"";
+    //var a=num+"";
     timerId=setInterval(fn
         ,2000)
 }
 
 //下标跟随
-function xiabiao(){
-    var figure=$('.lunbotu-xiabiao').children().length;
+function xiabiao(origin){
+    var figure=$('.'+origin+'>.lunbotu-xiabiao').children().length;
     for(var i=0;i<figure;i++){
-        $('.lunbotu-xiabiao').children().eq(i).on('mouseenter', function () {
+        $('.'+origin+'>.lunbotu-xiabiao').children().eq(i).on('mouseenter', function () {
             if(timerId){
                 clearInterval(timerId);
             }
             var indexLi=$(this).index();
 
             //console.log(indexLi)
-            $('.lunbotu-xiabiao').children().css({
+            $('.'+origin+'>.lunbotu-xiabiao').children().css({
                 backgroundColor:'white'
             })
             $(this).css({
                 backgroundColor:'black'
             })
-            $('.lunbotu-ul').animate({
+            $('.'+origin+'>.lunbotu-ul').animate({
                 marginLeft:(-inner_width*(indexLi+1))+'px'
             },200,'swing')
             index=indexLi;
         })
-        $('.lunbotu-xiabiao').children().eq(i).on('mouseleave', function () {
+        $('.'+origin+'>.lunbotu-xiabiao').children().eq(i).on('mouseleave', function () {
             timerId=setInterval(fn,2000)
         })
     }
--- lunbotu-VTEC/readMe.md
+++ lunbotu-VTEC/readMe.md
@@ -1,12 +1,11 @@
 #轮播图小插件说明
-###基于jQurey1.0版本，兼容IE6、7、8
+###基于jQurey1.0版本，兼容IE至8
 ####V1.0输入参数（图片数量,需要插入的div的类名），动态生成轮播图
 ####图片名称为"l"+num+'.jpg',例如l0.jpg.
 >```javascript
 >$(function () {
 >	var num=6
 >    var origin=document.querySelector('.lunbotu-		origin').className
->    alert(origin);
 >    console.log(origin)
 >    lunbotu(num,origin)
 >    autoMove(num)
